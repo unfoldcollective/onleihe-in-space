728277debb511ff5b60781bb46c7f4a4fc1b987f
Philo van Kemenade <philovankemenade@gmail.com>
2016-09-03 16:19:25 +0200

    added LedStrip Class

---
+++ LedStrip.cpp
@@ -0,0 +1,57 @@
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+
+// LedStrip Class wraps Adafruit_NeoPixel class to add custom animations
+
+class LedStrip {
+  Adafruit_NeoPixel leds;
+  
+  public:
+    LedStrip (int ledPin, int ledCount) {
+      leds = Adafruit_NeoPixel(ledCount, ledPin, NEO_GRB + NEO_KHZ800);
+    }
+
+    // Sets all LEDs to off, but DOES NOT update the display;
+    // call leds.show() to actually turn them off after this.
+    void clear()
+    {
+      for (int i=0; i<leds.numPixels(); i++)
+      {
+        leds.setPixelColor(i, 0);
+      }
+    }
+
+    void begin () {
+      leds.begin();
+    }
+
+    void show () {
+      leds.show();
+    }
+
+    uint32_t Color(int r, int g, int b){
+      return leds.Color(r, g, b);
+    }
+    
+    // Fill the dots one after the other with a color
+    void colorWipe(uint32_t c, uint8_t wait) {
+      for(uint16_t i=0; i<leds.numPixels(); i++) {
+        leds.setPixelColor(i, c);
+        leds.show();
+        delay(wait);
+      }
+    }
+
+    // Fill the dots one after the other with a color until specified number
+    void colorWipeUntil(uint32_t c, uint8_t wait, int until) {
+      for(uint16_t i=0; i<until; i++) {
+        leds.setPixelColor(i, c);
+        leds.show();
+        delay(wait);
+      }
+    }
+
+    
+    
+};
+
--- onleihe-in-space.ino
+++ onleihe-in-space.ino
@@ -1,15 +1,26 @@
 #include "OccupancyDetector.cpp"
+#include "LedStrip.cpp"
 
-OccupancyDetector detector(12, 13, 1000);
+OccupancyDetector detector = OccupancyDetector(12, 13, 1000);
+LedStrip leds = LedStrip(4, 119);
 
 void setup() {
   Serial.begin(9600);
+  
+  leds.begin();  // Call this to start up the LED strip.
+  leds.clear();  // turns all LEDs off...
+  leds.show();   // ...but the LEDs don't actually update until you call this.
 }
 
 void loop() {
   detector.Update();
-  int dis = detector.GetDistance();
   bool isOccupied = detector.isOccupied();
-  Serial.println(String(dis));
+  
   Serial.println(String(isOccupied));
+  
+  // TODO: trigger LED animation based on occupancy detected
+
+  leds.colorWipeUntil(leds.Color(255, 255, 255), 50, 60); // White
+  leds.colorWipeUntil(leds.Color(0, 255, 0), 50, 100); // Green
+  
 }
