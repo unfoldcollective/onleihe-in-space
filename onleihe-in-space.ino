d2eabc27eaf28863d1b0626487348ab4ebb4b853
Philo van Kemenade <philovankemenade@gmail.com>
2016-09-08 16:57:19 +0200

    turn detector off/on on detection

--- onleihe-in-space.ino
+++ onleihe-in-space.ino
@@ -3,6 +3,8 @@
 
 OccupancyDetector detector = OccupancyDetector(12, 13, 100);
 LedStrip leds = LedStrip(4, 120);
+bool isDetecting;
+bool hasDetected;
 
 void setup() {
   Serial.begin(9600);
@@ -10,34 +12,37 @@ void setup() {
   leds.begin();  // Call this to start up the LED strip.
   leds.clear();  // turns all LEDs off...
   leds.show();   // ...but the LEDs don't actually update until you call this.
+
+  isDetecting = true;
 }
 
 void loop() {
-  detector.update();
-  bool isOccupied = detector.isOccupied();
-  bool hasDetected = detector.detect();
+  if (isDetecting) {
+    detector.update();
+    hasDetected = detector.detect();
+    
+    Serial.println(hasDetected);
+    Serial.println("***");
   
-  Serial.println(isOccupied);
-  Serial.println(hasDetected);
-  Serial.println("***");
-
-  if(hasDetected){
-      // TODO: turn off detector
-      // detector.turnOff();
-
-      // animate leds up to level of amount
-      leds.colorWipeUntil(leds.Color(50, 50, 50), 50, 60); // White
-      
-      // pause
-      delay(3000);
-      
-      
-      // TODO: reverse wipe back to default state
-      // leds.colorWipeUntil(leds.Color(0, 100, 0), 50, 118);
-
-      leds.clear();
-      leds.show();
-      // TODO: turn on detector
-      // detector.turnOn();
-  }  
+    if(hasDetected){
+        // turn off detector
+         isDetecting = false;
+  
+        // animate leds up to level of amount
+        leds.colorWipeUntil(leds.Color(50, 50, 50), 50, 60); // White
+        
+        // pause
+        delay(3000);
+        
+        
+        // TODO: reverse wipe back to default state
+        // leds.colorWipeUntil(leds.Color(0, 100, 0), 50, 118);
+  
+        leds.clear();
+        leds.show();
+        
+        // turn on detector
+        isDetecting = true;
+    }  
+  }
 }
