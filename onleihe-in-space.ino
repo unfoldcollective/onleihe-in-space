b341177bfd06cddad3299e07d8e99b285e6d7171
Philo van Kemenade <philovankemenade@gmail.com>
2016-09-09 12:38:41 +0200

    refactor to bring define main parameters as #defined constants

--- onleihe-in-space.ino
+++ onleihe-in-space.ino
@@ -1,8 +1,23 @@
 #include "OccupancyDetector.cpp"
 #include "LedStrip.cpp"
 
-OccupancyDetector detector = OccupancyDetector(12, 13, 100);
-LedStrip leds = LedStrip(4, 120);
+#define TRIG_PIN 13
+#define ECHO_PIN 12
+#define DETECTOR_DELAY 100
+
+#define LED_PIN 4
+#define NO_LEDS 90
+
+// LED strip animation settings
+#define UP_TO 80
+#define LED_DELAY 50
+#define PAUSE_DELAY 3000
+
+OccupancyDetector detector = OccupancyDetector(ECHO_PIN, TRIG_PIN, DETECTOR_DELAY);
+LedStrip leds = LedStrip(LED_PIN, NO_LEDS);
+
+const uint32_t WHITE = leds.Color(50, 50, 50);
+
 bool isDetecting;
 bool hasDetected;
 
@@ -27,17 +42,15 @@ void loop() {
     if(hasDetected){
         // turn off detector
          isDetecting = false;
-  
+
         // animate leds up to level of amount
-        leds.colorWipeUntil(leds.Color(50, 50, 50), 50, 60); // White
+        leds.colorWipeUntil(WHITE, UP_TO, LED_DELAY); // White
         
         // pause
-        delay(3000);
+        delay(PAUSE_DELAY);
         
-        
-        // TODO: reverse wipe back to default state
-        // leds.colorWipeUntil(leds.Color(0, 100, 0), 50, 118);
-        leds.wipeOutFrom(60, 50);
+        // reverse wipe back to default state
+        leds.wipeOutFrom(UP_TO, LED_DELAY);
         
         // turn on detector
         isDetecting = true;
